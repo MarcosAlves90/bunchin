c5a69adfc6ae05ca2177bdec74920b6047d99bdd
MarcosAlves90 <MarcosAlves25@proton.me>
2024-09-22 05:19:44 -0300

    :sparkles: feat: update Administrador.jsx

--- React/src/pages/Administrador.jsx
+++ React/src/pages/Administrador.jsx
@@ -23,6 +23,56 @@ export default function Administrador() {
                         DADOS DO PERFIL
                     </h1>
                 </div>
+                <article className={"article-inputs"}>
+                    <div className={"article-inputs-input nome"}>
+                        <label>NOME COMPLETO</label>
+                        <input placeholder={"exemplo da silva paiva"} type={"text"}></input>
+                    </div>
+                    <div className={"article-inputs-input email"}>
+                        <label>EMAIL</label>
+                        <input placeholder={"exemplo@gmail.com"} type={"email"}></input>
+                    </div>
+                    <div className={"article-inputs-input n-registro"}>
+                        <label>Nº DE REGISTRO</label>
+                        <input placeholder={"1234567890"} type={"number"}></input>
+                    </div>
+                    <div className={"article-inputs-input senha"}>
+                        <label>SENHA</label>
+                        <input placeholder={"senhasegura1234"} type={"password"}></input>
+                    </div>
+                    <div className={"article-inputs-input cpf"}>
+                        <label>CPF</label>
+                        <input placeholder={"123.456.789-00"} type={"number"}></input>
+                    </div>
+                    <div className={"article-inputs-input funcao"}>
+                        <label>FUNÇÃO</label>
+                        <select defaultValue={"comum"}>
+                            <option value={"comum"}>Comum</option>
+                            <option value={"administrador"}>Administrador</option>
+                        </select>
+                    </div>
+                    <div className={"article-inputs-input cargo"}>
+                        <label>CARGO</label>
+                        <select defaultValue={"estagiario"}>
+                            <option value={"estagiario"}>Estagiário</option>
+                            <option value={"auxiliar-administrativo"}>Auxiliar administrativo</option>
+                            <option value={"gerente"}>Gerente</option>
+                            <option value={"diretor"}>Diretor</option>
+                        </select>
+                    </div>
+                    <div className={"article-inputs-input departamento"}>
+                        <label>DEPARTAMENTO</label>
+                        <select defaultValue={"administrativo"}>
+                            <option value={"administrativo"}>Administrativo</option>
+                            <option value={"financeiro"}>Financeiro</option>
+                            <option value={"marketing"}>Marketing</option>
+                            <option value={"producao"}>Produção</option>
+                        </select>
+                    </div>
+                </article>
+                <div className={"container-save-button"}>
+                    <button className={"save-button"}>Salvar alterações</button>
+                </div>
             </article>
         </main>
     )
