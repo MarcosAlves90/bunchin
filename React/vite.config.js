85b77d3b57b10d9e780661458ef64577e4e7f420
Marcos Alves <marcos.lopes26@fatec.sp.gov.br>
2024-11-04 19:41:28 -0300

    :sparkles: feat: starting host on vercel

--- React/vite.config.js
+++ React/vite.config.js
@@ -5,6 +5,7 @@ import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCommonjs()],
+  base: '/',
   optimizeDeps: {
     esbuildOptions: {
       plugins: [esbuildCommonjs(['react-moment'])],
