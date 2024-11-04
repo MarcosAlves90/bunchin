09724824c69b4a20d80ca1b71d3c72e07d1fe6f0
Marcos Alves <marcos.lopes26@fatec.sp.gov.br>
2024-11-04 20:36:01 -0300

    :bug: fix: test 01

--- React/vite.config.js
+++ React/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()],
+  plugins: [react()],
   base: '/',
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-moment'])],
-    },
-  },
 });
