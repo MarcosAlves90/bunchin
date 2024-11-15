d2175d650ef431ccab6213d77cac9b0bf98c2ccc
Marcos Alves <marcos.lopes26@fatec.sp.gov.br>
2024-11-14 21:06:14 -0300

    :sparkles: feat: update files

--- extra/bunchin_bd.sql
+++ extra/bunchin_bd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23/09/2024 às 22:01
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 14/11/2024 às 23:53
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,10 +43,11 @@ CREATE TABLE `tb_funcionario` (
 --
 
 INSERT INTO `tb_funcionario` (`n_registro`, `nome`, `email`, `senha`, `cpf`, `funcao`, `cargo`, `departamento`) VALUES
-('23523', 'Lyntter de Jesus Paiva', 'lyntter@gmail.com', 'lyntter', '12415251', 'administrador', 'auxiliar-administrativo', 'marketing'),
-('2342323', 'Kolovol da Silva Paiva Junior J', 'kolovolpaiva@gmail.com', 'Kolovol388', '73956473892', 'administrador', 'gerente', 'financeiro'),
-('324235256', 'Marcos Alves Lopes Paiva', 'marcoslopes@gmail.com', 'Homega3', '75647856234', 'administrador', 'diretor', 'marketing'),
-('89779832', 'Matheus Souza Paiva', 'matheus@gmail.com', 'Gaviota900', '86956478362', 'comum', 'estagiario', 'producao');
+('23523', 'Lyntter de Jesus Paiva', 'lyntter@gmail.com', '$2y$10$WQC7l/NMsq3id.axXoL4zeB9uMFbosrkPHB59.9NGvMxAPbIL5M.S', '12415251', 'administrador', 'auxiliar-administrativo', 'marketing'),
+('6213461', 'gaviota', 'gaviota@gmail.com', '$2y$10$dQ6WIbDwGv3OI/U1f1kLOu85AF1g8OUPQZ3HnzakcWGIRx/Yu33r.', '42634', 'administrador', 'auxiliar-administrativo', 'administrativo'),
+('2342323', 'Kolovol da Silva Paiva Junior J', 'kolovolpaiva@gmail.com', '$2y$10$ZaR9u484jQ5AQX.lfgP9n.rQ67l.iarOloAmS5ZpwVcrM50nLlwcm', '73956473892', 'administrador', 'gerente', 'financeiro'),
+('324235256', 'Marcos Alves Lopes Paiva', 'marcoslopes@gmail.com', '$2y$10$vQXZXvfK5Z.ykBmAgJG3kOMyYz9vhSSZFu3tM2uzkemQNRMb5juZe', '75647856234', 'administrador', 'diretor', 'marketing'),
+('89779832', 'Matheus Souza Paiva', 'matheus@gmail.com', '$2y$10$tjwhbUib0iRAMYu6zoeqRuDoO2Mj0WXNqZUuTPcuQji4yw23uvhN.', '86956478362', 'comum', 'estagiario', 'producao');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,11 @@ CREATE TABLE `tb_ponto` (
 
 INSERT INTO `tb_ponto` (`id_ponto`, `funcionario_fk`, `nome_tipo`, `data_hora`) VALUES
 ('26ad0267-d329-48b5-862d-52899f397ade', '12415251', 'Retorno', '2024-09-23T18:38:37.720Z'),
+('3b8de248-a4d1-47ff-89c4-ae3ec2fdb88b', '12415251', 'Almoço', '2024-11-05T22:29:05.337Z'),
+('6587dd37-61f2-4ce3-9bd7-f8d34d67204b', '12415251', 'Saída', '2024-11-05T22:29:06.505Z'),
 ('756d4315-cb5d-42d1-81aa-11a9864b6cc7', '12415251', 'Almoço', '2024-09-23T18:38:37.008Z'),
+('79a08748-ad72-4c2c-a4c3-95eabd3a434c', '12415251', 'Entrada', '2024-11-05T22:29:04.177Z'),
+('95f54e98-6194-4cc2-8319-e6ea8d0a82dd', '12415251', 'Retorno', '2024-11-05T22:29:05.913Z'),
 ('ab2458af-a540-4f27-a208-01fbe79224b5', '12415251', 'Saída', '2024-09-23T18:38:38.352Z'),
 ('d18cb737-9fd9-4b47-b71a-4929f3b547b3', '12415251', 'Entrada', '2024-09-23T18:38:35.896Z');
 
