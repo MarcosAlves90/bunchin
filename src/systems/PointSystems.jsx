58510ce485ba30ac5904519d169e0fb721e67f27
MarcosAlves90 <MarcosAlves25@proton.me>
2024-11-16 00:44:20 -0300

    :sparkles: feat: update PointSystems.jsx

--- src/systems/PointSystems.jsx
+++ src/systems/PointSystems.jsx
@@ -24,6 +24,7 @@ export function GeneratePoints({ registros, deletePonto }) {
     const handleCloseModal = useCallback(() => {
         setSelectedItem(null);
         setShowModal(false);
+        setCharCount(0);
     }, []);
 
     const handleTextareaChange = (event) => {
