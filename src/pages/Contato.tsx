20b15dfc852f4569715b2e0fd187610befc5afa6
MarcosAlves90 <MarcosAlves25@proton.me>
2025-06-03 23:18:31 -0300

    Refines UI styling and layout for consistency
    Updates layout and styling across Configurações, Contato, and Sobre pages to improve consistency and readability.
    
    Key changes:
    - Adjusts spacing, alignment, and container widths.
    - Refines text alignment and styling for better clarity.
    - Enhances hover effects for interactive elements.

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -10,23 +10,36 @@ export default function Contato() {
 
     return (
         <main className={`mainCommon text-base flex justify-center items-center flex-col`}>
-            <div className="container bg-card rounded-sm p-2 flex flex-col gap-1 text-primary transition-colors">
-                <h1 className="title text-4xl font-subrayada mb-1">CONTATE-NOS</h1>
-                <p className="description text-lg text-justify max-w-54 w-full mx-auto">
+            <div className="container rounded-sm p-2 flex flex-col gap-1 text-primary transition-colors max-w-60">
+                <h1 className="title text-4xl font-subrayada mb-2">CONTATE-NOS</h1>
+                <p className="description text-lg text-center w-full mx-auto">
                     Se você tiver alguma dúvida ou precisar de assistência, entre em contato conosco através das
                     seguintes opções:
                 </p>
-                <div className="contact-info text-base flex flex-col gap-0.5 transition-colors text-secondary">
-                    <a 
-                        href="https://maps.app.goo.gl/1i6jzv3SE2gBucKF7" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mx-auto max-w-54 w-full cursor-pointer bg-primary p-[0.2rem] rounded-sm duration-200 hover:bg-secondary hover:text-primary"
-                    >
-                        <strong>Endereço:</strong> Rua Exemplo, 123, Cidade, Estado, CEP 12345-678
-                    </a>
-                    
-                    <div className="mx-auto max-w-54 w-full">
+                <div className="contact-info text-base flex flex-col gap-0.5 transition-colors">
+                    <div className="wrapper flex-wrap flex flex-row mx-auto gap-0.5 w-full transition-colors">
+                        <a 
+                            href="https://maps.app.goo.gl/1i6jzv3SE2gBucKF7" 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mx-auto cursor-pointer p-[0.2rem] rounded-sm duration-200 hover:bg-secondary underline hover:font-bold"
+                        >
+                            <strong>Endereço:</strong> Rua Exemplo, 123, Cidade, Estado, CEP 12345-678
+                        </a>
+                        <a 
+                            href="tel:+551112345678"
+                            className="mx-auto cursor-pointer p-[0.2rem] duration-200 rounded-sm hover:bg-secondary underline hover:font-bold"
+                        >
+                            <strong>Telefone:</strong> (11) 1234-5678
+                        </a>
+                        <a 
+                            href="mailto:contato@bunchin.com"
+                            className="mx-auto cursor-pointer p-[0.2rem] duration-200 rounded-sm hover:bg-secondary underline hover:font-bold"
+                        >
+                            <strong>Email:</strong> contato@bunchin.com
+                        </a>
+                    </div>
+                    <div className="mx-auto w-full">
                         <MapView 
                             latitude={-23.6636053} 
                             longitude={-46.4601248} 
@@ -34,22 +47,9 @@ export default function Contato() {
                             height="13rem"
                         />
                     </div>
-                    <div className="wrapper flex flex-row mx-auto gap-0.5 max-w-54 w-full transition-colors">
-                        <a 
-                            href="tel:+551112345678"
-                            className="mx-auto max-w-54 w-full cursor-pointer bg-primary p-[0.2rem] duration-200 rounded-sm hover:bg-secondary hover:text-primary"
-                        >
-                            <strong>Telefone:</strong> (11) 1234-5678
-                        </a>
-                        <a 
-                            href="mailto:contato@bunchin.com"
-                            className="mx-auto max-w-54 w-full cursor-pointer bg-primary p-[0.2rem] duration-200  rounded-sm hover:bg-secondary hover:text-primary"
-                        >
-                            <strong>Email:</strong> contato@bunchin.com
-                        </a>
-                    </div>
+                    
                 </div>
-                <button className="button-home button-session mx-auto mt-1" onClick={handleHomeButtonClick}>
+                <button className="button-home button-session mx-auto mt-2" onClick={handleHomeButtonClick}>
                     Voltar para a página inicial
                 </button>
             </div>
--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -9,9 +9,9 @@ export default function Sobre() {
 
     return (
         <main className={`mainCommon text-base flex justify-center items-center flex-col`}>
-            <div className="container bg-card rounded-sm p-2 flex flex-col gap-1 text-primary transition-colors">
-                <h1 className="title text-4xl font-subrayada mb-1">Sobre Nós</h1>
-                <p className="description text-lg text-justify max-w-54 mx-auto">
+            <div className="container rounded-sm p-2 flex flex-col gap-3 text-primary transition-colors max-w-60">
+                <h1 className="title text-4xl font-subrayada">Sobre Nós</h1>
+                <p className="description text-lg text-justify mx-auto">
                     Bem-vindo ao Bunchin! Somos uma plataforma inovadora dedicada à gestão de ponto e dados de
                     funcionários. Nossa missão é transformar a maneira como as empresas gerenciam suas equipes,
                     oferecendo uma solução intuitiva, eficiente e confiável.<br/>No Bunchin, acreditamos que a tecnologia
@@ -19,7 +19,7 @@ export default function Sobre() {
                     que realmente importa: seu crescimento e sucesso. Junte-se a nós e descubra como podemos facilitar a
                     vida da sua empresa e de seus colaboradores.
                 </p>
-                <button className="button-home button-session mx-auto mt-1" onClick={handleHomeButtonClick}>
+                <button className="button-home button-session mx-auto" onClick={handleHomeButtonClick}>
                     Voltar para a página inicial
                 </button>
             </div>
